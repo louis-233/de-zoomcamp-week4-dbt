a6c601398550cc2524fcf2bbed7ceef3c32cea18
louis-233 <louis233lana@gmail.com>
2022-02-18 12:35:20 +0100

    fix

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -3,5 +3,5 @@
 /* select * from trips_data_all.yellow_tripdata; */
 
 select * 
-from {{ source('my_staging_gcp', 'trips_data_all') }}
+from {{ source('my_staging_gcp', 'yellow_tripdata_2019-01') }}
 limit 100;
