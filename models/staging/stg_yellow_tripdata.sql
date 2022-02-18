5c6b7b98eeb22c9f5d44725461de7d4616cca390
louis-233 <louis233lana@gmail.com>
2022-02-18 12:58:32 +0100

    update

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -1,5 +1,36 @@
 {{ config(materialized='view') }}
 
-/* select * from trips_data_all.yellow_tripdata; */
+select
+   -- identifiers
+    /* {{ dbt_utils.surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid, */
+    cast(vendorid as integer) as vendorid,
+    cast(ratecodeid as integer) as ratecodeid,
+    cast(pulocationid as integer) as  pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
+    
+    -- timestamps
+    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    store_and_fwd_flag,
+    cast(passenger_count as integer) as passenger_count,
+    cast(trip_distance as numeric) as trip_distance,
+    -- yellow cabs are always street-hail
+    1 as trip_type,
+    
+    -- payment info
+    cast(fare_amount as numeric) as fare_amount,
+    cast(extra as numeric) as extra,
+    cast(mta_tax as numeric) as mta_tax,
+    cast(tip_amount as numeric) as tip_amount,
+    cast(tolls_amount as numeric) as tolls_amount,
+    cast(0 as numeric) as ehail_fee,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(total_amount as numeric) as total_amount,
+    cast(payment_type as integer) as payment_type,
+    /* {{ get_payment_type_description('payment_type') }} as payment_type_description, */ 
+    cast(congestion_surcharge as numeric) as congestion_surcharge
 
-select * from {{ source('my_staging_gcp', 'yellow_tripdata_2019-01') }} limit 100
+from {{ source('my_staging_gcp', 'yellow_tripdata_2019-01') }}
+limit 100
