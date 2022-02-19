21c68a06a787be32bcfb1bbe157037aa066f4058
louis-233 <louis233lana@gmail.com>
2022-02-19 12:46:54 +0100

    update

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -29,6 +29,7 @@ select
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     cast(payment_type as integer) as payment_type,
+    {{ get_payment_type_description('payment_type') }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
 from {{ source('my_staging_gcp', 'yellow_tripdata_external_table') }}
